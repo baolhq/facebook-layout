54c28127bb7cbb22cd7b22a545ebe5fd4a421b3c
baolhq <quocbao.kaiser@gmail.com>
2022-05-15 20:29:13 +0700

    update UI

--- app/src/main/java/com/example/facebooklogin/MainActivity.kt
+++ app/src/main/java/com/example/facebooklogin/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.facebooklogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import com.example.facebooklogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +15,15 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        // Binding main activity view
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        //TODO: Set spinner text color
-        val adapter = ArrayAdapter.createFromResource(
+        // Set spinner items
+        ArrayAdapter(
             this,
-            R.array.language_array,
-            R.layout.color_spinner_layout // Dropdown item color
-        )
-        binding.selectLanguage.adapter = adapter
+            R.layout.color_spinner_layout, // Dropdown item color
+            resources.getStringArray(R.array.language_array),
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            val spinner = findViewById<Spinner>(R.id.select_language)
+            spinner.adapter = it
+        }
     }
 }
